eec126e1842deb605bbf6bb926ec8fa8f854cff8
Naga-Nag <lordofcarpinchus@gmail.com>
2025-05-28 19:48:58 -0300

    Updates

---
+++ _app/immutable/chunks/CAjv2RM_.js
@@ -0,0 +1,1 @@
+var s;const e=((s=globalThis.__sveltekit_2w8lmd)==null?void 0:s.base)??"/pruebas-fisicas-app";var a;const t=((a=globalThis.__sveltekit_2w8lmd)==null?void 0:a.assets)??e;export{t as a,e as b};
